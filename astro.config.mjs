5b316658289bf52079c4891a47f2a74ec4353581
icd_yokoo <yokoo@icd.co.jp>
2025-07-07 18:18:49 +0900

    changeing atro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://450k.github.io',
+    base: 'astro',
     vite: {
         plugins: [tailwindcss()]
     }
